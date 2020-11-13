0c38063c04271d1c980b161e6a589daa8d739196
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2020-11-13 14:20:14 -0600

    creating svg icons, banner and navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pt-10 w-full container mx-auto flex justify-between items-center">
+    <div>
+      <img src="/img/svg/menu.svg" alt="menu" class="h-6 w-6" />
+    </div>
+    <nav role="navigation" class="flex items-center">
+      <a href="" class="text-xl text-white font-semibold">Home</a>
+      <a href="" class="text-xl ml-10 text-white font-semibold">Smoothies</a>
+      <a href="" class="text-xl ml-10 text-white font-semibold">About</a>
+      <a href="" class="text-xl ml-10 text-white font-semibold">Contact</a>
+    </nav>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
